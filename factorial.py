4a97a9dd3e09c0e2f384f5dc76ae010f75260ffd
richellaod <36302856+richellaod@users.noreply.github.com>
2018-03-24 15:43:35 +0000

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,13 @@
+Richella O'Driscoll
+24.03.2018
+
+def factorial (i):
+    number = 1
+    while i >= 1:
+        number = number * i
+        i = i - 1
+    return number
+
+print (" the factorial of number 5 is:" , factorial (5))
+print (" the factorial of number 7 is:" , factorial (7))
+print (" the factorial of number 10 is:" , factorial (10))
